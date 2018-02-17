09d618f41effb77c129c58f1ff3927ee4727dc03
Michael Spohn <mikeLspohn@gmail.com>
2018-02-17 01:03:08 -0600

    Add rollup config and libs

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import pkg from './package.json'
+
+export default [
+	// browser-friendly UMD build
+	{
+		input: 'src/react-fetch.js',
+		output: {
+			name: 'react-fetch',
+			file: pkg.browser,
+			format: 'umd'
+		},
+		plugins: [
+			resolve(), // so Rollup can find `ms`
+			commonjs() // so Rollup can convert `ms` to an ES module
+		]
+	},
+
+    // esm/cjs builds
+	{
+		input: 'src/react-fetch.js',
+		external: ['ms'],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
+	}
+]
