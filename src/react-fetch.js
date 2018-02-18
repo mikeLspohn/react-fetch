f92fcddf5cb44e1b2df61f5cf19e3cd570e8cd29
Michael Spohn <mikeLspohn@gmail.com>
2018-02-17 18:35:04 -0600

    Setup eslint

--- src/react-fetch.js
+++ src/react-fetch.js
@@ -2,76 +2,77 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Fetch extends Component {
-    static propTypes = {
-      url: PropTypes.string.isRequired,
-      fetchOptions: PropTypes.object.isRequired, // @TODO: set shape to correct config shape for warning help
-      loading: PropTypes.func,
-      failure: PropTypes.func,
-      initial: PropTypes.func,
-      success: PropTypes.func
-    }
+  static propTypes = {
+    url: PropTypes.string.isRequired,
+    fetchOptions: PropTypes.object.isRequired, // @TODO: set shape to correct config shape for warning help
+    loading: PropTypes.func,
+    failure: PropTypes.func,
+    initial: PropTypes.func,
+    success: PropTypes.func,
+    children: PropTypes.oneOf([PropTypes.func])
+  }
 
-    static defaultProps = {
-      fetchOptions: Fetch.defaultOptions
-    }
+  static defaultProps = {
+    fetchOptions: Fetch.defaultOptions
+  }
 
-    static defaultOptions = {
-      method: 'get',
-      headers: {
-        'content-type': 'application/json'
-      }
+  static defaultOptions = {
+    method: 'get',
+    headers: {
+      'content-type': 'application/json'
     }
-  
-    constructor () {
-      super()
-      this.state = {
-        data: null,
-        err: null,
-        status: 'Initial' // 'Initial' | 'Loading' | 'Success' | 'Failure'
-      }
+  }
+
+  constructor () {
+    super()
+    this.state = {
+      data: null,
+      err: null,
+      status: 'Initial' // 'Initial' | 'Loading' | 'Success' | 'Failure'
     }
-  
-    componentDidMount () {
-      const { url, fetchOptions } = this.props
-      const mergedOptions = {...Fetch.defaultOptions, ...fetchOptions}
-  
-      this.setState({status: 'Loading'}, () => {
-        fetch(url, mergedOptions)
+  }
+
+  componentDidMount () {
+    const { url, fetchOptions } = this.props
+    const mergedOptions = {...Fetch.defaultOptions, ...fetchOptions}
+
+    this.setState({status: 'Loading'}, () => {
+      fetch(url, mergedOptions)
           .then(res => res.json())
           .then(data => this.setState({data, status: 'Success'}))
           .catch(err => this.setState({err, status: 'Failure'}))
-      })
-    }
-  
-    render () {
-      const { status, data, err } = this.state
+    })
+  }
+
+  render () {
+    const { status, data, err } = this.state
 
       // function-as-child support
-      if (typeof this.props.children === 'function') {
-        return this.props.children({status, data, err})
-      }
-  
+    if (typeof this.props.children === 'function') {
+      return this.props.children({status, data, err})
+    }
+
       // @TODO: Add render-prop support
       // @TODO: Add HOC support
 
       // Component Injection Support
-      const { loading, failure, initial, success } = this.props
-      const Initial = initial
-      const Success = success
-      const Failure = failure
-      const Loading = Loading
-  
-      switch (status) {
-        case 'Initial':
-          return <Initial />
-        case 'Loading':
-          return <Loading />
-        case 'Success':
-          return <Success data={data} />
-        case 'Failure':
-          return <Failure error={err} />
-        default:
-          return <Failure error='something went wrong' />
-      }
+    const { loading, failure, initial, success } = this.props
+    const Initial = initial
+    const Success = success
+    const Failure = failure
+    const Loading = loading
+
+    switch (status) {
+      case 'Initial':
+        return () => <Initial />
+      case 'Loading':
+        return <Loading />
+      case 'Success':
+        return <Success data={data} />
+      case 'Failure':
+        return <Failure error={err} />
+      default:
+        return <Failure error='something went wrong' />
     }
   }
+}
